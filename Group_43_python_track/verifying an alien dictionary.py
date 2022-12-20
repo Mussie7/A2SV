2556d7dec62e37a0839294cffd6e9376101c24a8
Mussie <90728686+Mussie7@users.noreply.github.com>
2022-12-20 22:43:39 +0300

    Create verifying an alien dictionary.py

---
+++ Group_43_python_track/verifying an alien dictionary.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        dic = {}
+        for i in range(len(order)):
+            dic[order[i]] = i+1
+        
+        cur = r = 0
+        while r < len(words):
+            if dic[words[r][0]] < cur:
+                return
+            
+            if dic[words[r][0]] == cur:
+                if words[r] in words[r-1] and len(words[r]) < len(words[r-1]):
+                    return
+                for i in range(min(len(words[r]), len(words[r-1]))):
+                    if dic[words[r][i]] < dic[words[r-1][i]]:
+                        return
+                    elif words[r][i] != words[r-1][i]:
+                        break
+
+            cur = dic[words[r][0]]                
+            r += 1
+        
+        return True
