f8c3709830cc7df604f2ac69059ef54593abf6a1
Mussie <90728686+Mussie7@users.noreply.github.com>
2022-12-29 18:26:28 +0300

    added another solution

--- Group_43_python_track/planets.py
+++ Group_43_python_track/planets.py
@@ -14,3 +14,27 @@ for _ in range(rep):
     n, c = tuple(map(int, input().split()))
     orbits = input().split()
     print(cheap_destruct(n, c, orbits))
+
+    
+from collections import Counter
+
+
+# same thing
+# def cheap_destruct(n, c, orbits):
+#     cost = 0
+#     value = Counter(orbits).most_common()
+    
+#     for i in range(len(value)):
+#         if value[i][1] == 1:
+#             cost += len(value) - i
+#             return cost
+        
+#         cost += min(value[i][1], c)
+    
+#     return cost
+
+# rep = int(input())
+# for _ in range(rep):
+#     n, c = tuple(map(int, input().split()))
+#     orbits = input().split()
+#     print(cheap_destruct(n, c, orbits))
