18539227863e5827a01cc14f6d658ecfed4096c8
Mussie <90728686+Mussie7@users.noreply.github.com>
2023-01-12 11:35:21 +0300

    Update delete columns to make sorted.py

--- Group_43_progress_sheet/delete columns to make sorted.py
+++ Group_43_progress_sheet/delete columns to make sorted.py
@@ -6,3 +6,15 @@ class Solution:
                 count += 1
 
         return count
+
+# better code in terms of space complexity
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        count = 0
+        for j in range(len(strs[0])):
+            for i in range(1, len(strs)):
+                if strs[i][j] < strs[i-1][j]:
+                    count += 1
+                    break
+        
+        return count
