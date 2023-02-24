2b8c7a799baf3959ec0ae9e41d8726c074d87c75
Mussie <90728686+Mussie7@users.noreply.github.com>
2023-02-24 17:44:07 +0300

    added a little faster(better?) solution

--- Group_43_camp_progress_sheet/longest repeating character replacement.py
+++ Group_43_camp_progress_sheet/longest repeating character replacement.py
@@ -23,3 +23,27 @@ class Solution:
             maxLen = max(maxLen, right-left+1)
         
         return maxLen
+
+# O(n) instead of O(26n)
+class Solution:
+    '''No need to decrement max frequency because I can't get a better one.
+    What is asked of me is the longest substring I can find by replacing k different characters and making the substring consist of only one character.
+    And I can only do that by finding the character with the most repetition that works by the previous rule.
+    '''
+    def characterReplacement(self, s: str, k: int) -> int:
+        left = 0
+        maxLen = 0
+        maxFreq = 0
+        counter = Counter()
+        for right in range(len(s)):
+            counter[s[right]] += 1
+            maxFreq = max(maxFreq, counter[s[right]])
+            while right - left + 1 - maxFreq > k:
+                counter[s[left]] -= 1
+                if counter[s[left]] == 0:
+                    del counter[s[left]]
+                left += 1
+            
+            maxLen = max(maxLen, right-left+1)
+        
+        return maxLen
