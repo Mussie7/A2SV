10a76d95b80e2f28bc0a1db06b5bb7127dcb7870
Mussie <90728686+Mussie7@users.noreply.github.com>
2023-03-04 00:04:46 +0300

    Create peak index in a mountain array.py

---
+++ Group_43_camp_progress_sheet/peak index in a mountain array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        left, right = 0, len(arr) - 1
+        while left + 1 < right:
+            mid = left + (right-left) // 2
+            if arr[mid] - arr[mid-1] > 0:
+                left = mid
+            else:
+                right = mid
+        
+        return left
