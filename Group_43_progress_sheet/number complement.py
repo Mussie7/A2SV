a1447d456a627e8742805411528598051feffb6b
Mussie <90728686+Mussie7@users.noreply.github.com>
2023-03-30 19:40:14 +0300

    Create number complement.py

---
+++ Group_43_progress_sheet/number complement.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        bitmask = 1
+        while bitmask <= num:
+            num ^= bitmask
+            bitmask <<= 1
+        
+        return num
