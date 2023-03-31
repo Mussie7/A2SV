6e8e088e62fed4e5656f41c588f8001ac24d9731
Mussie <90728686+Mussie7@users.noreply.github.com>
2023-03-31 23:13:42 +0300

    Added alternate solution

--- Group_43_progress_sheet/first missing positive.py
+++ Group_43_progress_sheet/first missing positive.py
@@ -1,4 +1,5 @@
 class Solution:
+    # cyclic sort
     def firstMissingPositive(self, nums: List[int]) -> int:
         for i in range(len(nums)):
             while 1 <= nums[i] <= len(nums) and nums[i] != i + 1:
@@ -13,3 +14,16 @@ class Solution:
             if num != i + 1:
                 return i + 1
         return len(nums) + 1
+    
+    # counting sort
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        counting = [0] * 100000
+        for num in nums:
+            if 0 < num < 100001:
+                counting[num-1] = 1
+        
+        for index, num in enumerate(counting):
+            if num == 0:
+                return index + 1
+        
+        return 100001
