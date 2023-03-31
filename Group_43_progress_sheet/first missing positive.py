7c8ba62232b6c354f0660a13cca58c02be839bc2
Mussie <90728686+Mussie7@users.noreply.github.com>
2023-03-31 23:17:29 +0300

    hash table solution

--- Group_43_progress_sheet/first missing positive.py
+++ Group_43_progress_sheet/first missing positive.py
@@ -27,3 +27,10 @@ class Solution:
                 return index + 1
         
         return 100001
+    
+    # hash table
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums = set(nums)
+        for i in range(1, len(nums) + 2):
+            if i not in nums:
+                return i
