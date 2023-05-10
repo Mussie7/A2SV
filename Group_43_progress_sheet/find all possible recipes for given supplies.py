4a5ec75169186bee04396e47f0db2889ac68cc8d
Mussie <90728686+Mussie7@users.noreply.github.com>
2023-05-10 18:53:59 +0300

    added a dfs solution

--- Group_43_progress_sheet/find all possible recipes for given supplies.py
+++ Group_43_progress_sheet/find all possible recipes for given supplies.py
@@ -1,3 +1,4 @@
+#Topological sort using bfs
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
         graph = defaultdict(set)
@@ -18,5 +19,30 @@ class Solution:
                 if ing_count[dependent] == 0:
                     cooked.append(dependent)
                     que.append(dependent)
+        
+        return cooked
+
+    
+# Topological sort using dfs
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        def dfs(food):
+            recipe_map[food][1] = 1
+            for gredient in ingredients[recipe_map[food][0]]:
+                if gredient in supplies or gredient in cooked:
+                    continue
+                elif gredient not in recipe_map or ((recipe_map[gredient][1] == 1 or not dfs(gredient))):
+                    return False
+            
+            cooked.add(food)
+            return True
+
+        supplies = set(supplies)
+        recipe_map = {recipe: [index, 0] for index, recipe in enumerate(recipes)}
+        cooked = set()
+
+        for recipe in recipes:
+            if recipe_map[recipe][1] == 0:
+                dfs(recipe)
         
         return cooked
