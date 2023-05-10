6af0616546c367fa2cc1218aac9762352ffea859
Mussie <90728686+Mussie7@users.noreply.github.com>
2023-05-10 18:12:40 +0300

    Added a bfs solution

--- Group_43_progress_sheet/course schedule.py
+++ Group_43_progress_sheet/course schedule.py
@@ -1,3 +1,4 @@
+# Topological sort using dfs
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         def dfs(course):
@@ -21,3 +22,27 @@ class Solution:
                 return False
         
         return True
+
+    
+# Topological sort using bfs
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        free = {i for i in range(numCourses)}
+        graph = [[] for _ in range(numCourses)]
+        req_counter = [0] * numCourses
+
+        for course, req in prerequisites:
+            graph[req].append(course)
+            free.discard(course)
+            req_counter[course] += 1
+        
+        que = deque(free)
+        while que:
+            course = que.popleft()
+            for sequel in graph[course]:
+                req_counter[sequel] -= 1
+                if req_counter[sequel] == 0:
+                    que.append(sequel)
+                    free.add(sequel)
+        
+        return len(free) == numCourses
