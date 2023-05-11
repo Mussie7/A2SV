9bb6b3e36735b998d99a7be2524ebaa9c3d08985
Mussie <90728686+Mussie7@users.noreply.github.com>
2023-05-11 22:22:32 +0300

    added a faster solution.py

--- Group_43_progress_sheet/remove stones to minimize the total.py
+++ Group_43_progress_sheet/remove stones to minimize the total.py
@@ -11,3 +11,26 @@ class Solution:
             k -= 1
 
         return -sum(piles)
+
+# faster, cooler solution
+class Solution:
+    def minStoneSum(self, piles: List[int], k: int) -> int:
+        piles_count = defaultdict(int)
+        max_pile = piles_total = 0
+        for pile in piles:
+            max_pile = max(pile, max_pile)
+            piles_total += pile
+            piles_count[pile] += 1
+        
+        pile = max_pile
+        while k:
+            if piles_count[pile] > 0:
+                piles_count[pile] -= 1
+                k -= 1
+                amount = pile // 2
+                piles_total -= amount
+                piles_count[pile - amount] += 1
+            else:
+                pile -= 1
+
+        return piles_total
