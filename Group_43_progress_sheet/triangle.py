08e393ee5f3c87088aa8d14a470bb941d05af041
Mussie <90728686+Mussie7@users.noreply.github.com>
2023-06-04 14:09:00 +0300

    Added an alternate solution

--- Group_43_progress_sheet/triangle.py
+++ Group_43_progress_sheet/triangle.py
@@ -1,3 +1,4 @@
+# O(1) space and O(n^2) time complexity
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         for i in range(len(triangle) - 2, -1, -1):
@@ -5,3 +6,16 @@ class Solution:
                 triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1])
         
         return triangle[0][0]
+
+    
+    
+# a solution with out overwriting the values of the input
+# O(n) space and O(n^2) time complexity
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        dp = triangle[-1].copy()
+        for i in range(len(triangle) - 2, -1, -1):
+            for j in range(len(triangle[i])):
+                dp[j] = triangle[i][j] + min(dp[j], dp[j+1])
+        
+        return dp[0]
