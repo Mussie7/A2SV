bf1c97aa3a8327d02fc96d3c8a223fa7906a64b1
Mussie <90728686+Mussie7@users.noreply.github.com>
2023-07-19 00:55:21 +0300

    added a O(n) solution

--- Group_43_progress_sheet/wiggle subsequence.py
+++ Group_43_progress_sheet/wiggle subsequence.py
@@ -11,3 +11,19 @@ class Solution:
 
         n = len(nums)
         return max(dp(0, 1), dp(0, -1))
+
+
+
+# O(n) solution
+# the number of turning points is directly related to the number of elements in the wiggle subsequence
+# turning point - change in sign of difference between adjacent elements
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        length = 1
+        sign = 0
+        for i in range(1, len(nums)):
+            if (nums[i] - nums[i-1]) != 0 and (nums[i] - nums[i-1]) // abs(nums[i] - nums[i-1]) != sign:
+                length += 1
+                sign = (nums[i] - nums[i-1]) // abs(nums[i] - nums[i-1])
+        
+        return length
