2bc8fd4e9b157d24d853c99ecf60153b688344c1
Mussie <90728686+Mussie7@users.noreply.github.com>
2023-10-05 22:01:37 +0300

    added a bottom up approach implementation

--- Group_43_camp_2_progress_sheet/uncrossed lines.py
+++ Group_43_camp_2_progress_sheet/uncrossed lines.py
@@ -1,3 +1,4 @@
+# top_down approach
 class Solution:
     def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
         @cache
@@ -14,3 +15,20 @@ class Solution:
             return lines
         
         return dp(0, 0)
+
+
+# bottom-up approach
+class Solution:
+    def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
+        n, m = len(nums1), len(nums2)
+        dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+        for i in range(n - 1, -1, -1):
+            for j in range(m - 1, -1, -1):
+                # chooseing the best with out making a line with the current indices
+                dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+                
+                # checking whether we can make a line with the current indices
+                if nums1[i] == nums2[j]:
+                    dp[i][j] = max(dp[i][j], dp[i+1][j+1] + 1)
+        
+        return dp[0][0]
