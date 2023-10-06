6e786e7d37075b6871a840fb89fd89802965acc7
Mussie <90728686+Mussie7@users.noreply.github.com>
2023-10-06 11:06:51 +0300

    Update path sum III.py

--- Group_43_camp_2_progress_sheet/path sum III.py
+++ Group_43_camp_2_progress_sheet/path sum III.py
@@ -7,21 +7,19 @@
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
         def dfs(node, curr_sum, target_sum):
+            if not node:
+                return 0
+
             curr_sum += node.val
             path_count = pre_sum_map[curr_sum - target_sum]
             pre_sum_map[curr_sum] += 1
-            if node.left:
-                path_count += dfs(node.left, curr_sum, target_sum)
-            if node.right:
-                path_count += dfs(node.right, curr_sum, target_sum)
-
-            # decrement the curr_sum count in the pre_sum_map
+            
+            path_count += dfs(node.left, curr_sum, target_sum)
+            path_count += dfs(node.right, curr_sum, target_sum)
+            
             pre_sum_map[curr_sum] -= 1
             return path_count
         
-        if not root:
-            return 0
-
         pre_sum_map = defaultdict(int)
         pre_sum_map[0] = 1
         return dfs(root, 0, targetSum)
