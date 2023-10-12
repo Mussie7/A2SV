4de2b6e7ea701eacc011d548eb9b75e2a9ff9187
Mussie <90728686+Mussie7@users.noreply.github.com>
2023-10-12 14:49:00 +0300

    add an alternate implementation

--- Group_43_camp_2_progress_sheet/minimum number of operations to make array continuous.py
+++ Group_43_camp_2_progress_sheet/minimum number of operations to make array continuous.py
@@ -1,4 +1,5 @@
 class Solution:
+    # binary search implementation
     def minOperations(self, nums: List[int]) -> int:
         n = len(nums)
         nums = sorted(set(nums))
@@ -6,5 +7,22 @@ class Solution:
         for i in range(len(nums)):
             idx = bisect.bisect_left(nums, nums[i] + n)
             operations = min(operations, i + len(nums) - idx)
+        
+        return operations + n - len(nums)
+
+    # sliding window implementation
+    def minOperations(self, nums: List[int]) -> int:
+        n = len(nums)
+        nums = sorted(set(nums))
+        operations = n
+        right = 0
+        for left in range(len(nums)):
+            end = nums[left] + n - 1
+            while right < len(nums) and nums[right] <= end:
+                right += 1
+            
+            operations = min(operations, left + len(nums) - right)
+            if right == n:
+                break
         
         return operations + n - len(nums)
